1f55bcc87b1df2ba6ec7c9bb2490b9f0bdb24d7f
bet00n <luka9pl@gmail.com>
2023-01-21 17:13:15 +0100

    Feat about me section

--- script.js
+++ script.js
@@ -1,71 +1,88 @@
-const intro = document.querySelector('.intro');
-const main = document.querySelector('main');
-const aboutMeBtn = document.querySelector('.about-me-btn');
-const aboutMeSection = document.querySelector('.about-me');
-const hobbiesBtn = document.querySelector('.hobbies-btn');
-const hobbiesSection = document.querySelector('.hobbies');
-const skillsBtn = document.querySelector('.skills-btn');
-const skillsSection = document.querySelector('.skills');
-const projectsBtn = document.querySelector('.projects-btn');
-const projectsSection = document.querySelector('.projects');
-const closeSectionBtns = document.querySelectorAll('.close-section-btn');
+const intro = document.querySelector(".intro");
+const main = document.querySelector("main");
+const aboutMeBtn = document.querySelector(".about-me-btn");
+const aboutMeSection = document.querySelector(".about-me");
+const hobbiesBtn = document.querySelector(".hobbies-btn");
+const hobbiesSection = document.querySelector(".hobbies");
+const skillsBtn = document.querySelector(".skills-btn");
+const skillsSection = document.querySelector(".skills");
+const projectsBtn = document.querySelector(".projects-btn");
+const projectsSection = document.querySelector(".projects");
+const closeSectionBtns = document.querySelectorAll(".close-section-btn");
+const skillsBtns = document.querySelectorAll(".skill");
 
+const updateTime = () => {
+  const startDate = new Date("6/7/2022");
+  const now = new Date();
+  const diffTime = Math.abs(now - startDate);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  document.querySelector(".counter").innerHTML = diffDays;
+  setTimeout(() => {
+    updateTime();
+  }, 1000);
+};
+
+updateTime();
 
 const toggleMain = () => {
-    main.classList.toggle('hidden');
-}
+  main.classList.toggle("hidden");
+};
 
-intro.addEventListener('click',() => {
-    intro.querySelector('.top').classList.add('slide-top');
-    intro.querySelector('.bottom').classList.add('slide-bottom');
-    setTimeout(() => {
-        intro.style.display = 'none';
-    }, 300);
-})
-
-const openSection = (e, section) => {
-    const targetCover = e.target;
-    console.log(targetCover);
-    targetCover.querySelector('.cover').classList.add('cover-page');
-    setTimeout(() => {
-        toggleMain();
-        targetCover.querySelector('.cover').classList.remove('cover-page');
-        section.style.display = 'block';
-    }, 1000)
-}
-
-aboutMeBtn.addEventListener('click', (e) => {
-    openSection(e, aboutMeSection);
+intro.addEventListener("click", () => {
+  intro.querySelector(".top").classList.add("slide-top");
+  intro.querySelector(".bottom").classList.add("slide-bottom");
+  setTimeout(() => {
+    intro.style.display = "none";
+  }, 300);
 });
 
-hobbiesBtn.addEventListener('click', (e) => {
-    openSection(e, hobbiesSection);
-})
+const openSection = (e, section) => {
+  const targetCover = e.target;
+  targetCover.querySelector(".cover").classList.add("cover-page");
+  setTimeout(() => {
+    toggleMain();
+    targetCover.querySelector(".cover").classList.remove("cover-page");
+    section.style.display = "flex";
+  }, 1000);
+};
 
-skillsBtn.addEventListener('click', (e) => {
-    openSection(e, skillsSection);
-})
+aboutMeBtn.addEventListener("click", (e) => {
+  openSection(e, aboutMeSection);
+});
 
-projectsBtn.addEventListener('click', (e) => {
-    openSection(e, projectsSection);
-})
+hobbiesBtn.addEventListener("click", (e) => {
+  openSection(e, hobbiesSection);
+});
+
+skillsBtn.addEventListener("click", (e) => {
+  openSection(e, skillsSection);
+});
+
+projectsBtn.addEventListener("click", (e) => {
+  openSection(e, projectsSection);
+});
 
 closeSectionBtns.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-        const targetSection = e.target.closest('section');
-        console.log(targetSection);
-        targetSection.classList.add('slide-wide');
-        toggleMain();
-        setTimeout(() => {
-            targetSection.style.display = 'none';
-            targetSection.classList.remove('slide-wide');
-        },2000);
-    })
-})
+  btn.addEventListener("click", (e) => {
+    const targetSection = e.target.closest("section");
+    targetSection.classList.add("slide-wide");
+    toggleMain();
+    setTimeout(() => {
+      targetSection.style.display = "none";
+      targetSection.classList.remove("slide-wide");
+    }, 2000);
+  });
+});
+
+document.querySelectorAll(".flip-card").forEach((card) => {
+  card.addEventListener("click", (e) => {
+    e.target.closest(".flip-card-inner").classList.toggle("rotated");
+  });
+});
 
 
-document.querySelectorAll('.flip-card').forEach((card) => {
-    card.addEventListener('click', (e) => {
-        e.target.closest('.flip-card-inner').classList.toggle('rotated');
-    })
+skillsBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    console.log(e);
+  })
 })
